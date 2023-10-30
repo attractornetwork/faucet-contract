b8a518e2a1f34a69534a7b081bde3239293dcd88
roflganker <abominati0n@protonmail.com>
2023-10-30 15:07:35 +0000

    Finished deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,21 +3,20 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function main() {
-  const checkpoint = Math.round(new Date().getTime() / 1000);
+  const checkpoint = Math.round(Date.now() / 1000);
   const [deployer] = await ethers.getSigners();
   console.log(`About to deploy Faucet from ${deployer.address}`);
   const balance = await deployer.getBalance();
   console.log(`Deployer balance is ${ethers.utils.formatEther(balance)}`);
 
-  const wallet = ethers.Wallet.createRandom();
-  const signer = await wallet.getAddress();
-  const token = process.env.FAUCET_TOKEN_ADDRESS || '0x' + '0'.repeat(40);
-  const portion = process.env.FAUCET_TOKEN_PORTION || '1' + '0'.repeat(18);
-  const Faucet = await ethers.getContractFactory("Faucet");
-  const faucet = await Faucet.deploy(signer, token, portion);
+  const signer = ethers.Wallet.createRandom();
+  const token = ethers.utils.getAddress(process.env.FAUCET_TOKEN_ADDRESS || 'nonsense');
+  const portion = ethers.BigNumber.from(process.env.FAUCET_TOKEN_PORTION || 'kitten');
+  const Faucet = await ethers.getContractFactory('Faucet');
+  const faucet = await Faucet.deploy(token, portion, signer.address);
   const deployment = faucet.deployTransaction;
   console.log(`Deployment started. Transaction hash is ${deployment.hash}`);
-  const confirmations = parseInt(process.env.MIN_CONFIRMATIONS ?? '2'); 
+  const confirmations = parseInt(process.env.MIN_CONFIRMATIONS ?? 'fill .env bro');
   console.log(`Waiting for ${confirmations} confirmations...`);
   await deployment.wait(2);
   console.log(`The job is done :)`);
@@ -30,18 +29,18 @@ async function main() {
         chainId: network.config.chainId,
       },
       address: faucet.address,
-      token: await faucet.callStatic.token(),
-      portion: (await faucet.callStatic.portion()).toString(),
+      token: await faucet.token(),
+      portion: (await faucet.portion()).toString(),
       deployer: deployment.from,
-      owner: await faucet.callStatic.owner(),
+      owner: await faucet.owner(),
       signer: {
-        address: signer,
-        privateKey: wallet.privateKey,
+        address: signer.address,
+        privateKey: signer.privateKey,
       },
     },
   };
   const filekey = `faucet-${checkpoint}.json`;
-  const filename = path.join(__filename, '../deploy', filekey);
+  const filename = path.join(process.cwd(), 'deploy', filekey);
   fs.writeFileSync(filename, JSON.stringify(doc, undefined, 2));
   console.log(`Deployment info has beed saved to ${filename}`);
 }
