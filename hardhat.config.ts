0dc58e57625588e0699f5fc82edb6eb19ccbb77f
roflganker <abominati0n@protonmail.com>
2023-10-30 15:00:52 +0000

    Prepared deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,50 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+function getenv(key: string): string {
+  const env = process.env[key];
+  if (!env) throw new Error(`Missing process.env.${key}`);
+  return env;
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.20',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    attratest: {
+      chainId: 9701,
+      url: getenv('ATTRA_TESTNET_URL'),
+      accounts: [getenv('ACCOUNT_PRIV_KEY')],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      attratest: getenv('ATTRA_EXPLORER_API_KEY'),
+    },
+    customChains: [
+      {
+        chainId: 9701,
+        network: 'attratest',
+        urls: {
+          apiURL: 'https://explorer.dev.attra.me/api/',
+          browserURL: 'https://explorer.dev.attra.me/',
+        },
+      }
+    ]
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,49 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const checkpoint = Math.round(new Date().getTime() / 1000);
+  const [deployer] = await ethers.getSigners();
+  console.log(`About to deploy Faucet from ${deployer.address}`);
+  const balance = await deployer.getBalance();
+  console.log(`Deployer balance is ${ethers.utils.formatEther(balance)}`);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const wallet = ethers.Wallet.createRandom();
+  const signer = await wallet.getAddress();
+  const token = process.env.FAUCET_TOKEN_ADDRESS || '0x' + '0'.repeat(40);
+  const portion = process.env.FAUCET_TOKEN_PORTION || '1' + '0'.repeat(18);
+  const Faucet = await ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy(signer, token, portion);
+  const deployment = faucet.deployTransaction;
+  console.log(`Deployment started. Transaction hash is ${deployment.hash}`);
+  const confirmations = parseInt(process.env.MIN_CONFIRMATIONS ?? '2'); 
+  console.log(`Waiting for ${confirmations} confirmations...`);
+  await deployment.wait(2);
+  console.log(`The job is done :)`);
+  console.log(`Saving info...`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const doc = {
+    faucet: {
+      network: {
+        name: network.name,
+        chainId: network.config.chainId,
+      },
+      address: faucet.address,
+      token: await faucet.callStatic.token(),
+      portion: (await faucet.callStatic.portion()).toString(),
+      deployer: deployment.from,
+      owner: await faucet.callStatic.owner(),
+      signer: {
+        address: signer,
+        privateKey: wallet.privateKey,
+      },
+    },
+  };
+  const filekey = `faucet-${checkpoint}.json`;
+  const filename = path.join(__filename, '../deploy', filekey);
+  fs.writeFileSync(filename, JSON.stringify(doc, undefined, 2));
+  console.log(`Deployment info has beed saved to ${filename}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
